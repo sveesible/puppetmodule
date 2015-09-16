d67e893b34d28c5630fbdbb02e69d6748073fb4a
Brad Svee <brad.svee@gmail.com>
2015-09-16 08:17:40 -0700

    Create passenger.pp
    mod_passenger is available at this repository

---
+++ manifests/repo/passenger.pp
@@ -0,0 +1,24 @@
+class puppet::repo::passenger{
+
+ if($::osfamily == 'Debian') {
+    apt::source { "passenger_repo":
+      location    => "https://oss-binaries.phusionpassenger.com/apt/passenger",
+      key         => {
+        id     => "561F9B9CAC40B2F7",
+        server => "hkp://keyserver.ubuntu.com:80" 
+      },
+      include_src => false,
+    }
+ } elsif $::osfamily == 'Redhat' {
+     yumrepo {'passenger':
+       baseurl =>'https://oss-binaries.phusionpassenger.com/yum/passenger/el/$releasever/$basearch',
+       gpgcheck => 0,
+       enabled => 1,
+       gpgkey => 'https://packagecloud.io/gpg.key',
+       sslverify=> 1,
+       sslcacert=>'/etc/pki/tls/certs/ca-bundle.crt'
+     }
+  else {
+    fail("Unsupported osfamily ${::osfamily}")
+  }
+}
